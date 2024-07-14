ffcc771196d35a7e93448ceb6ffb8bbb5a3d310a
sheikhishmam2002 <latifurahman2@gmail.com>
2024-07-14 00:28:34 -0700

    The Simon Says Game gile

---
+++ script.js
@@ -0,0 +1,87 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "yellow", "green", "purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector('h2');
+
+document.addEventListener('keypress', function(){
+    if(started == false){
+        console.log("Game is started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add('gameFlash');
+    setTimeout(function(){
+        btn.classList.remove('gameFlash');
+    },380);
+}
+function userFlash(btn){
+    btn.classList.add('userFlash');
+    setTimeout(function(){
+        btn.classList.remove('userFlash');
+    },250)
+}
+function levelUp(){
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * btns.length);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+function checkAns(idx){
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+            document.querySelector('.correct').style.backgroundColor = "green";
+            setTimeout(function(){
+                document.querySelector('.correct').style.backgroundColor = "#94FF9B";
+                
+            },700);
+        }
+    }else{
+        h2.innerHTML = `<b>Game Over!</b> Your score was <b>${level}</b>. <br> Press any key to start.`;
+        document.querySelector('.wrong').style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector('.wrong').style.backgroundColor = "#FF9D97";
+        },1000);
+        reset();
+    }
+}
+
+function btnPress(){
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    console.log(userSeq);
+
+    checkAns(userSeq.length - 1);
+}
+
+let allBtn = document.querySelectorAll('.btn');
+for(btn of allBtn){
+    btn.addEventListener('click',btnPress);
+}
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
